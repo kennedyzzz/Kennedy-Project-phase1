78e347cec005e01555d32a012501a2d34d7ca64c
kennedy <kitongakennedy9@gmail.com>
2024-04-12 15:36:00 +0300

    done

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,6 @@
 let order = [];
 let total = 0;
-
+//This enables us to click any item and it will be viewed in the order summary together with the price
 document.querySelectorAll("li").forEach((item) => {
   item.addEventListener("click", () => {
     let name = item.getAttribute("data-name");
@@ -11,13 +11,22 @@ document.querySelectorAll("li").forEach((item) => {
   });
 });
 
+//This makes it that when ther order button is clicked it displays a text and the total
 document.getElementById("order-btn").addEventListener("click", () => {
-  alert("Thank You Order placed! Total: $ " + total.toFixed(2));
+  alert("Order placed! Total: $ " + total.toFixed(2));
   order = [];
   total = 0;
   updateOrderSummary();
 });
+//This is to enable us to get a respnse once the submit comment is clicked
+const ComButton = document.getElementsByClassName("btn");
+for (let i = 0; i < ComButton.length; i++) {
+  ComButton[i].addEventListener("click", () => {
+    alert("Your comment has been sent. Thank you for your support.");
+  });
+}
 
+//This makes it possible to see all the individual items in our order summary
 function updateOrderSummary() {
   let orderList = document.getElementById("order-list");
   let totalElement = document.getElementById("total");
@@ -30,7 +39,10 @@ function updateOrderSummary() {
   totalElement.textContent = total.toFixed(2);
 }
 
-const base_url = "https://my-json-server.typicode.com/kennedyzzz/Kennedy-Project-phase1/Menu";
+const base_url =
+  "https://my-json-server.typicode.com/kennedyzzz/Kennedy-Project-phase1/Menu";
+  
+// a function used for fetching info from json server
 
 function fetching() {
   fetch(`${base_url}`, {
